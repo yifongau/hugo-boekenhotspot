d7962084a58b64ee1b5d8d2f0ccdf51a5a2eff6a
Yi Fong Au <yifongau@gmail.com>
2026-05-02 23:38:18 +0200

    More efficient parsing of book files

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,43 @@
 function renderBooksGrid(books, booksGridId) {
-    const booksGrid=document.getElementById(booksGridId);
+    const booksGrid = document.getElementById(booksGridId);
 
-    if ( !booksGrid) {
+    if (!booksGrid) {
         throw new Error(`${booksGridId} element not found`);
     }
 
     // Iterate over books and populate the books grid
-    books.forEach((book)=> {
+    books.forEach((book) => {
 
-            // Construct link element
-            const link=document.createElement("a");
-            link.className = "book-card";
-            link.href=book.bookFilePath;
-            link.target="_blank"; // Open in new tab
-            link.rel="noopener noreferrer"; // Security best practice
+        // Construct link element
+        const link = document.createElement("a");
+        link.className = "book-card";
+        link.href = book.bookFilePath;
+        link.target = "_blank"; // Open in new tab
+        link.rel = "noopener noreferrer"; // Security best practice
 
-            // Construct image element
-            const img = document.createElement("img");
-            img.className = "book-cover";
-            img.src=book.imageFilePath;
-            img.alt="Boekomslag"; // Alt text for accessibility 
-            img.loading="lazy"; // Lazy load images for better performance
-            img.decoding="async"; // Asynchronous decoding for better performance
-         
-            // Wrap <img> in <a> to make the entire image clickable.
-            link.appendChild(img);
+        // Construct image element
+        const img = document.createElement("img");
+        img.className = "book-cover";
+        img.src = book.imageFilePath;
+        img.alt = "Boekomslag"; // Alt text for accessibility 
+        img.loading = "lazy"; // Lazy load images for better performance
+        img.decoding = "async"; // Asynchronous decoding for better performance
 
-            const item=document.createElement("div");
-            item.appendChild(link);
-            booksGrid.appendChild(item);
-        });
+        // Wrap <img> in <a> to make the entire image clickable.
+        link.appendChild(img);
 
-       console.log("Book files:", books);
+        const item = document.createElement("div");
+        item.appendChild(link);
+        booksGrid.appendChild(item);
+    });
+
+    console.log("Book files:", books);
+
+    books.forEach((book) => {
+        if (!book.bookFilePath) console.log(book);
+        if (!book.imageFilePath) console.log(book);
+        if (!book.metadata) console.log(book);
+    });
+
 
 }
